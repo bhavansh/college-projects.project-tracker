e49369c772dfb19f05c7f63d4e4a2871c56c2b2c
saurabh-619 <saurabhbomble107@gmail.com>
2020-11-18 17:22:48 +0530

    Remote url for db added.

--- core-application/helper/db.js
+++ core-application/helper/db.js
@@ -7,3 +7,12 @@ mongoose
   })
   .then((msg) => console.log("Database connected successfully."))
   .catch((err) => console.log(err));
+
+// mongoose.connect(
+//   process.env.MONGO_ATLAS_URI,
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then((msg) => console.log("Database connected successfully."))
+//   .catch((err) => console.log(err));
